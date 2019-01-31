0fb39f01414569bdd7a4e8a0f01bf3e799ea980b
Chris Crow <crow@offtherecord.com>
2019-01-31 14:59:36 -0800

    Updates

---
+++ precache-manifest.b975c34ecbe042246177094896f62dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffc231b7b1e54bf1cee",
+    "url": "/TV-Journal/static/js/runtime~main.dffc231b.js"
+  },
+  {
+    "revision": "09bdc24ed559d01ce342",
+    "url": "/TV-Journal/static/js/main.09bdc24e.chunk.js"
+  },
+  {
+    "revision": "86e2201326bab55b0d3a",
+    "url": "/TV-Journal/static/js/1.86e22013.chunk.js"
+  },
+  {
+    "revision": "09bdc24ed559d01ce342",
+    "url": "/TV-Journal/static/css/main.c02413aa.chunk.css"
+  },
+  {
+    "revision": "86e2201326bab55b0d3a",
+    "url": "/TV-Journal/static/css/1.f7f86765.chunk.css"
+  },
+  {
+    "revision": "e445d3b5f80bb9b99cecaaaa8f3a20be",
+    "url": "/TV-Journal/index.html"
+  }
+];
